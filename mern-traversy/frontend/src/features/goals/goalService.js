a1dd4ebe8a4eaa73dcc29839e7bf4c66fb43eb09
Siriuz <72621548+thirdyraza@users.noreply.github.com>
2022-10-26 19:23:07 +0800

    delete in CRUD
    final addition, i guess. there's still the deployment of the app.

--- mern-traversy/frontend/src/features/goals/goalService.js
+++ mern-traversy/frontend/src/features/goals/goalService.js
@@ -24,9 +24,21 @@ const getGoals = async(token) =>{
     return response.data
 }
 
+//delete goal
+const deleteGoal = async(goalId, token) =>{
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.delete(API_URL + goalId, config)
+    return response.data
+}
+
 const goalService = {
     createGoal,
     getGoals,
+    deleteGoal,
 }
 
 export default goalService
--- mern-traversy/frontend/src/features/goals/goalSlice.js
+++ mern-traversy/frontend/src/features/goals/goalSlice.js
@@ -32,6 +32,17 @@ export const getGoals = createAsyncThunk('goals/getAll', async(_, thunkAPI) => {
     }
 })
 
+// delete user goal
+export const deleteGoal = createAsyncThunk('goals/delete', async(id, thunkAPI) =>{
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await goalService.deleteGoal(id, token)
+    } catch (error) {
+        const message = (error.reponse && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
 export const goalSlice = createSlice({
     name: 'goal',
     initialState,
@@ -62,6 +73,19 @@ export const goalSlice = createSlice({
                 state.goals = action.payload
             })
             .addCase(getGoals.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+            .addCase(deleteGoal.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(deleteGoal.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals = state.goals.filter((goal) => goal._id !== action.payload.id)
+            })
+            .addCase(deleteGoal.rejected, (state, action) => {
                 state.isLoading = false
                 state.isError = true
                 state.message = action.payload
